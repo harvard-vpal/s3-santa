ef058eb7aa18b5d5ad2312f6e2e48fe25e0a9d0f
Andrew Ang <ang.kunanit@gmail.com>
2017-09-01 15:30:52 -0400

    Refactor to use boto3 instead of aws cli for api calls

--- s3santa/aws.py
+++ s3santa/aws.py
@@ -1,51 +1,44 @@
-import os
+"""
+Utility module for calling aws endpoints and parsing results
+"""
+
 import json
+import boto3
 
 
-def run(cmd,parse=True):
-    """
-    Run a system command and optionally return the result as a dictionary
-    """
-    if parse:
-        with os.popen(cmd) as f:
-            try:
-                cmd_result_parsed = json.load(f)
-            except ValueError:
-                raise ValueError(f.read()) 
-        return cmd_result_parsed
-    else:
-        status = os.system(cmd)
-        if status is not 0:
-            # run again to print the error
-            run(cmd,parse=True)
+iam = boto3.client('iam')
+s3 = boto3.client('s3')
     
 
-def list_users(group=None):
-    """
-    Returns a list of usernames
-    """
-    if not group:
-        results = run("aws iam list-users")
-    else:
-        results = run("aws iam get-group")
-    usernames = [user['UserName'] for user in results['Users']]
-    return usernames
+# def list_users(group=None):
+#     """
+#     Returns a list of usernames that exist in the aws account
+#     Arguments:
+#         group (str): if included, 
+#     """
+#     if not group:
+#         results = iam.list_users()
+#     else:
+#         results = iam.get_group()
+#     usernames = [user['UserName'] for user in results['Users']]
+#     return usernames
+
 
 def create_user(username):
     """
     Creates a new IAM user and returns the username
     """
-    results = run("aws iam create-user --user-name {}".format(username))
-    # username = results['User']['UserName']
+    results = iam.create_user(UserName=username)
+    username = results['User']['UserName']
     print "Created IAM user: {}".format(username)
-    return results
+    return username
     
 
 def create_access_key(username):
     """
     Creates an access key pair for an IAM user
     """
-    results = run("aws iam create-access-key --user-name {}".format(username))
+    results = iam.create_access_key(UserName=username)
     access_key = {
         'AccessKeyId':results['AccessKey']['AccessKeyId'],
         'SecretAccessKey':results['AccessKey']['SecretAccessKey']
@@ -58,59 +51,58 @@ def get_user(username):
     """
     Get info about an existing user
     """
-    results = run("aws iam get-user --user-name {}".format(username))
+    results = iam.get_user(UserName=username)
     return results
+
 
 def user_exists(username):
     """
-    Returns True if the user exists
+    Returns True if the user exists, False if they do not exist
     """
-    usernames = list_users()
-    return bool(username in usernames)
+    try: 
+        get_user(username)
+        return True
+    except iam.exceptions.NoSuchEntityException:
+        return False
+
 
 def add_user_to_group(username, group):
     """
-    attach user to group
+    Attach user to IAM group
     """
-    results = run(
-        "aws iam add-user-to-group --user-name {} --group-name {}".format(
-            username,
-            group,
-        ),
-        parse=False
-    )
+    results = iam.add_user_to_group(UserName=username, GroupName=group)
     print "User {} added to group {}".format(username,group)
 
-def upload_to_s3(file,bucket,username):
+
+def upload_to_s3(file, bucket, username, key_suffix=None):
     """
-    upload a file to a user's folder
+    Upload a file to a user's folder.
+    Key format is:
+        s3://<bucket_name>/<username>/<key_suffix>
     """
+    # check if local file exists
     if not os.path.exists(file):
         raise ValueError("Specified file or directory not found")
 
-    results = run(
-        "aws s3 cp {} s3://{}/{}/ {}".format(
-            file,
-            bucket,
-            username,
-            '--recursive' if os.path.isdir(file) else ''
-        ),
-        parse=False
-    )
+    if key_suffix:
+        key = "{}/{}".format(username, key_suffix)
+    else:
+        key = username
+
+    s3.upload_file(file, bucket, key)
+    print "Uploaded file {} to s3://{}/{}".format(file, bucket, key)
+
 
 def create_s3_folder(bucket,foldername):
     """
     Create a folder on s3
     """
-    results = run(
-        "aws s3api put-object --bucket {} --key {}/".format(
-            bucket,
-            foldername,
-        ),
-        parse=False
+    results = s3.put_object(
+        Bucket=bucket,
+        Key="{}/".format(foldername)
     )
     print "Created folder {}".format(foldername)
 
+
 if __name__=="__main__":
     pass
-
