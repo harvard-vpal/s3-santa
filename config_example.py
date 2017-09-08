a61ce687f8a5578c9bb6d1c36a7246b66ea573d5
Andrew Ang <ang.kunanit@gmail.com>
2017-09-08 17:34:41 -0400

    Example script to run santa cli

---
+++ config_example.py
@@ -0,0 +1,16 @@
+"""
+This is a Example private config file
+Copy this file and rename to config.py
+Script santa.py references variables here
+"""
+# name of S3 bucket
+S3_BUCKET = 'my-bucket'
+
+# iam group
+IAM_GROUP = 'test-users'
+
+# location of google service account credential file
+GOOGLE_KEYFILE = '~/my-project-abc123.json'
+
+# file id of google spreadsheet
+GOOGLE_SPREADSHEET_ID = 'asdf12345'
